628d0397a02cf70e1cfe60bc6499f858411315c9
jobeuse <iradukundajob3@gmail.com>
2022-03-11 13:20:24 -0800

    last digi

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	else if (l == 0)
 	{
 		printf("Last digit of %i is %i and is 0 \n", n, l);
-	}	
+	}
 	else if ((l < 6) && (l != 0))
 	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
