58ad18dcc8f2a3e35ffc8f135fcc5af9946a061d
jobeuse <iradukundajob3@gmail.com>
2022-03-11 14:30:10 -0800

    alphapetic

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
-{	
+{
 	char cha;
-	for(cha = 'a'; cha <= 'z'; cha++)
+	
+	for (cha = 'a'; cha <= 'z'; cha++)
 	{
 		putchar(cha);
 	}
